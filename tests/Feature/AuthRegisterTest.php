b15c4a79710954571516bb3bcf198921b30a29b6
Felix Penrose <felixpenrose@gmail.com>
2020-08-16 17:09:44 +0100

    Test for registering a new user

--- tests/Feature/AuthRegisterTest.php
+++ tests/Feature/AuthRegisterTest.php
@@ -2,12 +2,18 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\User;
 use Tests\TestCase;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Event;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class AuthRegisterTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
     public function register_route_has_correct_fields()
     {
@@ -22,4 +28,28 @@ class AuthRegisterTest extends TestCase
         $response->assertSuccessful();
         $response->assertViewIs('auth.register');
     }
+
+
+
+    /** @test */
+    public function a_user_can_register()
+    {
+        $password = 'Testing123!#';
+
+        $response = $this->post('/register', [
+            'name' => 'John Doe',
+            'email' => 'john@testing.com',
+            'password' => $password,
+            'password_confirmation' => $password,
+        ]);
+
+        $this->assertCount(1, $users = User::all());
+        $this->assertAuthenticatedAs($user = $users->first());
+        $this->assertEquals('John Doe', $user->name);
+        $this->assertEquals('john@testing.com', $user->email);
+        $this->assertTrue(Hash::check($password, $user->password));
+
+        $response->assertRedirect(RouteServiceProvider::HOME);
+    }
+
 }
--- tests/Feature/AuthResetPasswordTest.php
+++ tests/Feature/AuthResetPasswordTest.php
@@ -2,12 +2,19 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\User;
 use Tests\TestCase;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class AuthResetPasswordTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
     public function reset_password_route_has_correct_fields()
     {
@@ -20,5 +27,40 @@ class AuthResetPasswordTest extends TestCase
         ]);
         $response->assertSuccessful();
         $response->assertViewIs('auth.passwords.email');
+    }
+
+
+    /** @test */
+    public function user_receives_an_email_with_a_password_reset_link()
+    {
+        Notification::fake();
+
+        $user = factory(User::class)->create();
+
+        $response = $this->post('/password/email', [
+            'email' => $user->email,
+        ]);
+
+        $this->assertNotNull($token = DB::table('password_resets')->first());
+
+        Notification::assertSentTo($user, ResetPassword::class, function ($notification, $channels) use ($token) {
+            return Hash::check($notification->token, $token->token) === true;
+        });
+    }
+
+
+    /** @test */
+    public function user_does_not_receive_email_when_not_registered()
+    {
+        Notification::fake();
+
+        $response = $this->from('/password/reset')->post('/password/email', [
+            'email' => 'nobody@example.com',
+        ]);
+
+        $response->assertRedirect('/password/reset');
+        $response->assertSessionHasErrors('email');
+
+        Notification::assertNotSentTo(factory(User::class)->make(['email' => 'nobody@example.com']), ResetPassword::class);
     }
 }
