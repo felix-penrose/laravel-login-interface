414b05792748275a967d993dd66f4f07ab4cab36
Felix Penrose <felixpenrose@gmail.com>
2020-08-20 14:29:36 +0100

    adding missing files that didn't show in the last commit

--- app/User.php
+++ app/User.php
@@ -19,6 +19,7 @@ class User extends Authenticatable
         'first_name',
         'last_name',
         'email',
+        'password',
         'username',
         'personal_site',
         'location',
